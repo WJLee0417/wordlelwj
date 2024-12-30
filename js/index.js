fd6cd5d00756b87e6e33b17a656bbf32ba58e734
WJLee0417 <dnjswns041797@gmail.com>
2024-12-30 19:03:01 +0900

    [워들] 글자 출력 크기 상향, 내부 프로그램 구현

--- js/index.js
+++ js/index.js
@@ -1,6 +1,53 @@
+const 정답 = "APPLE";
+
+let attempts = 0;
+let index = 0;
+
 function appStart() {
-  const handleKeydown = () => {
-    console.log("키가 눌렸습니다!");
+  const gameover = () => {
+    window.removeEventListener("keydown", handleKeydown);
+  };
+
+  const nextLine = () => {
+    if (attempts === 6) return;
+    attempts += 1;
+    index = 0;
+  };
+
+  const handleEnterKey = () => {
+    let 맞은_개수 = 0;
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-block[data-index='${attempts}${i}']`
+      );
+      const 입력한_글자 = block.innerText;
+      const 정답_글자 = 정답[i];
+      if (입력한_글자 === 정답_글자) {
+        맞은_개수 += 1;
+        block.style.background = "#6AAA64";
+      } else if (정답.includes(입력한_글자)) block.style.background = "#C9B458";
+      else block.style.background = "#787C7E";
+      block.style.color = "white";
+    }
+
+    if (맞은_개수 === 5) gameover();
+    else nextLine();
+  };
+  const handleKeydown = (event) => {
+    const key = event.key.toUpperCase();
+    const keyCode = event.keyCode;
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+
+    if (index === 5) {
+      if (event.key === "Enter") handleEnterKey();
+      else return;
+    } else if (event.key === "Enter") handleEnterKey();
+    else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index += 1;
+    }
   };
 
   window.addEventListener("keydown", handleKeydown);
